20cbd7625bc6c4c1a6693c5bd692aa06b807991a
Stefan Miklosovic <stefan.miklosovic@instaclustr.com>
2020-04-28 22:15:27 +0200

    introduced flag for overriding the resoultion of running in K8S

--- src/main/java/com/instaclustr/kubernetes/KubernetesHelper.java
+++ src/main/java/com/instaclustr/kubernetes/KubernetesHelper.java
@@ -9,7 +9,7 @@ public class KubernetesHelper {
     public static final String KUBERNETES_SERVICE_PORT = "KUBERNETES_SERVICE_PORT";
 
     public static boolean isRunningInKubernetes() {
-        return System.getenv(KUBERNETES_SERVICE_HOST) != null && System.getenv(KUBERNETES_SERVICE_PORT) != null;
+        return System.getenv(KUBERNETES_SERVICE_HOST) != null && System.getenv(KUBERNETES_SERVICE_PORT) != null && !isPretendingToRunOutsideKubernetes();
     }
 
     public static boolean isRunningAsClient() {
@@ -18,6 +18,10 @@ public class KubernetesHelper {
 
     public static String getHostname() throws UnknownHostException {
         return InetAddress.getLocalHost().getHostName();
+    }
+
+    public static boolean isPretendingToRunOutsideKubernetes() {
+        return Boolean.parseBoolean(System.getProperty("pretend.not.running.in.kubernetes", "false"));
     }
 
     public static String getPodName() throws UnknownHostException {
