878eb1d8fee8c8e4fdb8bec4beb5b5f717056724
Stefan Miklosovic <stefan.miklosovic@instaclustr.com>
2020-05-05 11:01:56 +0200

    implement functionality for retrieving of ColumnFamilyStoreMBeans

--- src/main/java/jmx/org/apache/cassandra/service/CassandraJMXService.java
+++ src/main/java/jmx/org/apache/cassandra/service/CassandraJMXService.java
@@ -5,14 +5,19 @@ import static javax.management.JMX.newMBeanProxy;
 import javax.management.MBeanServerConnection;
 import javax.management.ObjectName;
 import javax.management.remote.JMXConnector;
+import java.util.Set;
 
 import com.instaclustr.operations.FunctionWithEx;
 import jmx.org.apache.cassandra.CassandraJMXConnectionInfo;
 import jmx.org.apache.cassandra.JMXUtils;
+import jmx.org.apache.cassandra.service.cassandra3.ColumnFamilyStoreMBean;
 import jmx.org.apache.cassandra.service.cassandra3.StorageServiceMBean;
+import jmx.org.apache.cassandra.service.cassandra4.Cassandra4ColumnFamilyStoreMBean;
 import jmx.org.apache.cassandra.service.cassandra4.Cassandra4StorageServiceMBean;
 
 public interface CassandraJMXService {
+
+    // storage service mbean
 
     <T> T doWithCassandra4StorageServiceMBean(FunctionWithEx<Cassandra4StorageServiceMBean, T> func) throws Exception;
 
@@ -20,10 +25,16 @@ public interface CassandraJMXService {
 
     <T> T doWithStorageServiceMBean(FunctionWithEx<StorageServiceMBean, T> func) throws Exception;
 
-    default <T, U> T doWithStorageServiceMBean(FunctionWithEx<U, T> func,
-                                               Class<U> mbeanClass,
-                                               ObjectName objectName,
-                                               CassandraJMXConnectionInfo jmxConnectionInfo) throws Exception {
+    // column family store mbean
+
+    <T> T doWithCassandra3ColumnFamilyStoreMBean(FunctionWithEx<ColumnFamilyStoreMBean, T> func, String keyspace, String columnFamily) throws Exception;
+
+    <T> T doWithCassandra4ColumnFamilyStoreMBean(FunctionWithEx<Cassandra4ColumnFamilyStoreMBean, T> func, String keyspace, String columnFamily) throws Exception;
+
+    default <T, U> T doWithMBean(FunctionWithEx<U, T> func,
+                                 Class<U> mbeanClass,
+                                 ObjectName objectName,
+                                 CassandraJMXConnectionInfo jmxConnectionInfo) throws Exception {
 
         try (JMXConnector jmxConnector = JMXUtils.getJmxConnector(jmxConnectionInfo)) {
 
@@ -32,6 +43,33 @@ public interface CassandraJMXService {
             MBeanServerConnection mBeanServerConnection = jmxConnector.getMBeanServerConnection();
 
             return func.apply(newMBeanProxy(mBeanServerConnection, objectName, mbeanClass));
+        }
+    }
+
+    default <T, U> T doWithMBean(FunctionWithEx<U, T> func,
+                                 Class<U> mbeanClass,
+                                 String query,
+                                 CassandraJMXConnectionInfo jmxConnectionInfo) throws Exception {
+
+        try (JMXConnector jmxConnector = JMXUtils.getJmxConnector(jmxConnectionInfo)) {
+
+            jmxConnector.connect();
+
+            MBeanServerConnection mBeanServerConnection = jmxConnector.getMBeanServerConnection();
+
+            Set<ObjectName> objectNames = mBeanServerConnection.queryNames(new ObjectName(query), null);
+
+            if (objectNames.isEmpty()) {
+                throw new IllegalStateException(String.format("Could not find ObjectName with query %s", query));
+            }
+
+            if (objectNames.size() != 1) {
+                throw new IllegalStateException(String.format("There is more than one ObjectName returned by query %s. They are: %s",
+                                                              query,
+                                                              objectNames.stream().map(ObjectName::getCanonicalName)));
+            }
+
+            return func.apply(newMBeanProxy(mBeanServerConnection, objectNames.iterator().next(), mbeanClass));
         }
     }
 }
--- src/main/java/jmx/org/apache/cassandra/service/CassandraJMXServiceImpl.java
+++ src/main/java/jmx/org/apache/cassandra/service/CassandraJMXServiceImpl.java
@@ -3,7 +3,9 @@ package jmx.org.apache.cassandra.service;
 import com.instaclustr.operations.FunctionWithEx;
 import jmx.org.apache.cassandra.CassandraJMXConnectionInfo;
 import jmx.org.apache.cassandra.CassandraObjectNames;
+import jmx.org.apache.cassandra.service.cassandra3.ColumnFamilyStoreMBean;
 import jmx.org.apache.cassandra.service.cassandra3.StorageServiceMBean;
+import jmx.org.apache.cassandra.service.cassandra4.Cassandra4ColumnFamilyStoreMBean;
 import jmx.org.apache.cassandra.service.cassandra4.Cassandra4StorageServiceMBean;
 
 public class CassandraJMXServiceImpl implements CassandraJMXService {
@@ -15,17 +17,47 @@ public class CassandraJMXServiceImpl implements CassandraJMXService {
     }
 
     @Override
-    public <T> T doWithCassandra4StorageServiceMBean(FunctionWithEx<Cassandra4StorageServiceMBean, T> func) throws Exception {
-        return doWithStorageServiceMBean(func, Cassandra4StorageServiceMBean.class, CassandraObjectNames.V4.STORAGE_SERVICE_MBEAN_NAME, jmxConnectionInfo);
+    public <T> T doWithCassandra4StorageServiceMBean(final FunctionWithEx<Cassandra4StorageServiceMBean, T> func) throws Exception {
+        return doWithMBean(func,
+                           Cassandra4StorageServiceMBean.class,
+                           CassandraObjectNames.V4.STORAGE_SERVICE_MBEAN_NAME,
+                           jmxConnectionInfo);
     }
 
     @Override
-    public <T> T doWithCassandra3StorageServiceMBean(FunctionWithEx<StorageServiceMBean, T> func) throws Exception {
+    public <T> T doWithCassandra3StorageServiceMBean(final FunctionWithEx<StorageServiceMBean, T> func) throws Exception {
         return doWithStorageServiceMBean(func);
     }
 
     @Override
-    public <T> T doWithStorageServiceMBean(FunctionWithEx<StorageServiceMBean, T> func) throws Exception {
-        return doWithStorageServiceMBean(func, StorageServiceMBean.class, CassandraObjectNames.V3.STORAGE_SERVICE_MBEAN_NAME, jmxConnectionInfo);
+    public <T> T doWithStorageServiceMBean(final FunctionWithEx<StorageServiceMBean, T> func) throws Exception {
+        return doWithMBean(func,
+                           StorageServiceMBean.class,
+                           CassandraObjectNames.V3.STORAGE_SERVICE_MBEAN_NAME,
+                           jmxConnectionInfo);
+    }
+
+    @Override
+    public <T> T doWithCassandra3ColumnFamilyStoreMBean(final FunctionWithEx<ColumnFamilyStoreMBean, T> func,
+                                                        final String keyspace,
+                                                        final String columnFamily) throws Exception {
+        return doWithMBean(func, ColumnFamilyStoreMBean.class,
+                           getColumnFamilyMBeanObjectNameQuery(keyspace, columnFamily),
+                           jmxConnectionInfo);
+    }
+
+    @Override
+    public <T> T doWithCassandra4ColumnFamilyStoreMBean(final FunctionWithEx<Cassandra4ColumnFamilyStoreMBean, T> func,
+                                                        final String keyspace,
+                                                        final String columnFamily) throws Exception {
+        return doWithMBean(func,
+                           Cassandra4ColumnFamilyStoreMBean.class,
+                           getColumnFamilyMBeanObjectNameQuery(keyspace, columnFamily),
+                           jmxConnectionInfo);
+    }
+
+    private String getColumnFamilyMBeanObjectNameQuery(final String keyspace, final String columnFamily) {
+        final String type = columnFamily.contains(".") ? "IndexColumnFamilies" : "ColumnFamilies";
+        return "org.apache.cassandra.db:type=*" + type + ",keyspace=" + keyspace + ",columnfamily=" + columnFamily;
     }
 }
