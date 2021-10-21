5154ba6d8d699c2e37db833fbcd41b09a20e16f7
Stefan Miklosovic <smiklosovic@apache.org>
2021-10-21 09:16:07 +0200

    add EndpointSnitchInfoMBean for Cassandra 2

--- src/main/java/jmx/org/apache/cassandra/CassandraObjectNames.java
+++ src/main/java/jmx/org/apache/cassandra/CassandraObjectNames.java
@@ -7,6 +7,7 @@ public final class CassandraObjectNames {
 
     public static class V2 {
         public static final ObjectName STORAGE_SERVICE_MBEAN_NAME = ObjectNames.create("org.apache.cassandra.db:type=StorageService");
+        public static final ObjectName ENDPOINT_SNITCH_INFO_MBEAN_NAME = ObjectNames.create("org.apache.cassandra.db:type=EndpointSnitchInfo");
     }
 
     public static class V3 {
